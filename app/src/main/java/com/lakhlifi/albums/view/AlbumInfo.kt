28e203ab75bc5a02b524a25eec1d22ff036029c2
Essaddiq Lakhlifi <elakhlifi@smb.sqli.com>
2021-03-19 10:48:26 +0100

    viewbinding

--- app/src/main/java/com/lakhlifi/albums/view/AlbumInfo.kt
+++ app/src/main/java/com/lakhlifi/albums/view/AlbumInfo.kt
@@ -5,11 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
-//import androidx.databinding.DataBindingUtil
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.lakhlifi.albums.R
-//import com.lakhlifi.albums.databinding.ActivityAlbumInfoBinding
+import com.lakhlifi.albums.databinding.ActivityAlbumInfoBinding
 import com.lakhlifi.albums.network.model.Album
 import com.lakhlifi.albums.viewModel.AlbumInfoViewModel
 import com.lakhlifi.albums.viewModel.AlbumViewModel
@@ -19,24 +19,24 @@ import com.squareup.picasso.Picasso
 
 
 class AlbumInfo : AppCompatActivity() {
-    //lateinit var binding:ActivityAlbumInfoBinding
+    lateinit var binding:ActivityAlbumInfoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
 
         //get setContentView from DataBindingUtil
-        //binding= DataBindingUtil.setContentView(this,R.layout.activity_album_info)
-        setContentView(R.layout.activity_album_info)
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_album_info)
+        //setContentView(R.layout.activity_album_info)
 
         //views with binding
-        /*val album_title_binding=binding.albumTitle
+        val album_title_binding=binding.albumTitle
         val album_image_binding=binding.imageInfo
         val id_user_binding=binding.idUser
-        val id_album_binding=binding.idAlbum*/
+        val id_album_binding=binding.idAlbum
 
         //views
-        val album_title=findViewById<TextView>(R.id.album_title)
-        val image_info=findViewById<ImageView>(R.id.image_info)
+        //val album_title=findViewById<TextView>(R.id.album_title)
+        //val image_info=findViewById<ImageView>(R.id.image_info)
         val id_album=findViewById<TextView>(R.id.id_Album)
         val id_user=findViewById<TextView>(R.id.id_user)
 
@@ -47,14 +47,14 @@ class AlbumInfo : AppCompatActivity() {
         //filter using the method getAlbum
         albumInfoViewModel.getAlbum(application,id)
         albumInfoViewModel.album.observe(this, Observer{
-            album_title.setText(it.title)
+            album_title_binding.setText(it.title)
            Picasso
                .get()
                .load("https://picsum.photos/id/${it.id}/200")
                .memoryPolicy(MemoryPolicy.NO_CACHE, MemoryPolicy.NO_STORE)
                .networkPolicy(NetworkPolicy.NO_CACHE, NetworkPolicy.NO_STORE)
                .placeholder(R.drawable.image)
-               .into(image_info);
+               .into(album_image_binding);
             //id_album_binding.setText(""+it.id)
             id_album.apply {
                 text= it.id.toString()
